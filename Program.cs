937d8aa4ac7711d1a31d3bb48203a54705a6272c
ibrabro <ibrox9656@gmail.com>
2017-12-20 04:25:45 +0300

    Create table 'students'

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Npgsql;
 
 namespace BDProkopyev
 {
@@ -6,7 +7,28 @@ namespace BDProkopyev
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var connString = "Host=localhost;Username=ibrabro;Password=qwerty;Database=timetable";
+
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+
+                // Insert some data
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+                    
+                    cmd.CommandText = "INSERT INTO students (name, phone, address) VALUES ('Иванов Иван Иванович', 2432323, 'DU, 3/1, 201')";
+                    // cmd.Parameters.AddWithValue("p", "Hello world");
+                    cmd.ExecuteNonQuery();
+                }
+
+                // Retrieve all rows
+                using (var cmd = new NpgsqlCommand("SELECT name FROM students", conn))
+                using (var reader = cmd.ExecuteReader())
+                    while (reader.Read())
+                        Console.WriteLine(reader.GetString(0));
+            }
         }
     }
 }

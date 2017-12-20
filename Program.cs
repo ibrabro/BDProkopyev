e24fc820be5729ea036fe0ab7a9945e4acc967b9
ibrabro <ibrox9656@gmail.com>
2017-12-20 03:08:11 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BDProkopyev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
